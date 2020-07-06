8a110cc8ea09770c852dea4e5f7a4bc0efd33c6c
SeongGyeong <deborahoh94@gmail.com>
2020-07-06 13:17:21 +0900

    Initial commit

---
+++ app/src/main/java/com/osg/project01bookdiary/MainActivity.java
@@ -0,0 +1,14 @@
+package com.osg.project01bookdiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
